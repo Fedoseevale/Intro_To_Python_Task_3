38f2d4597ed5dccfe58125f75b2ad5781d9c6687
Александр <fedoseevale@gmail.com>
2022-11-29 22:54:30 +0400

    Execise 1 done

---
+++ Execise1.py
@@ -0,0 +1,16 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка
+# с нечетными индексами.
+# Пример: [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+import random
+
+size = int(input('Введите размер списка: '))
+my_list = []
+for i in range(size):
+    my_list.append(random.randint(0, 10))
+print(my_list)
+
+sumElWithOddIndexes = 0
+for i in range(1, size, 2):
+    sumElWithOddIndexes += my_list[i]
+print(sumElWithOddIndexes)
+
